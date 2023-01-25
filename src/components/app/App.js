273bddc61e58c2bc96b87e347e056076d0b51b96
SardorDev <sardorqobilov555@gmail.com>
2023-01-25 11:50:02 +0500

    new commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,26 +6,41 @@ import MoviesAddForm from '../movies-add-form/MoviesAddForm';
 import SearchPanel from '../search-panel/SearchPanel';
 import './app.css'
 
-const App = () => {
-  const data = [
-    { name: 'Empire of Osman', viewers: 726, id: 1, favourite: false },
-    { name: 'Ertugrul', viewers: 856, id: 2, favourite: false },
-    { name: 'Osman', viewers: 999, id: 3, favourite: false },
-  ]
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: 'Empire of Osman', viewers: 726, id: 1, favourite: false },
+        { name: 'Ertugrul', viewers: 856, id: 2, favourite: false },
+        { name: 'Osman', viewers: 999, id: 3, favourite: false },
+      ]
+    }
+  }
 
-  return (
-    <div className='app font-monospace'>
-      <div className="content">
-        <AppInfo />
-        <div className='search-panel'>
-          <SearchPanel />
-          <AppFilter />
+  onDeleted = (id) => {
+    const index = this.data.findIndex(item => item.id !== id);
+    console.log(index);
+    // this.setState({
+
+    // })
+  }
+
+  render() {
+    return (
+      <div className='app font-monospace'>
+        <div className="content">
+          <AppInfo />
+          <div className='search-panel'>
+            <SearchPanel />
+            <AppFilter />
+          </div>
+          <MovieList data={this.state.data} onDeleted={this.onDeleted} />
+          <MoviesAddForm />
         </div>
-        <MovieList data={data} />
-        <MoviesAddForm />
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default App;
--- src/components/movie-list/MovieList.jsx
+++ src/components/movie-list/MovieList.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import MovieListItem from '../movie-list-item/MovieListItem';
 import './movie-list.css'
 
-const MovieList = ({data}) => {
+const MovieList = ({data, onDeleted}) => {
   return (
     <ul className='movie-list'>
       {data.map(item => (
-        <MovieListItem key={item.id} {...item}/>
+        <MovieListItem key={item.id} {...item} onDeleted={onDeleted}/>
       ))}
     </ul>
   );
